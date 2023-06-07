69882051cbabdbf99c29cafa51c1cdcc44d18c86
thiagobatistagomes <122994560+thiagobatistagomes@users.noreply.github.com>
2023-06-07 13:02:23 -0300

    tentativa ex10

---
+++ exercicio10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+    Ler duas strings A e B e mostrar quantas vezes a string A ocorre dentro da string B.
+*/
+
+int main ()
+{
+    char strA[100], strB[100];
+    int cont = 0, i , j;
+
+    scanf(" %[^\n]", strA);
+    scanf(" %[^\n]", strB);
+
+    i = strlen(strA);
+    j = strlen(strB);
+
+    //verificar quantas vezes a string A ocorre dentro de B
+
+    
+
+
+    return 0;
+}
